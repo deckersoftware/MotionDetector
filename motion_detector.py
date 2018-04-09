023b4f217365388492674565f11033d6116b034f
Christian Decker <christian.decker@decker-software-solutions.de>
2018-04-09 16:07:25 +0200

    inital commit

---
+++ motion_detector.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+
+import RPi.GPIO as GPIO
+import time
+import threading
+
+GPIO.setmode(GPIO.BOARD)
+
+GPIO_PIR = 7
+
+GPIO.setup(GPIO_PIR, GPIO.IN)
+
+timer_thread = None
+hdmi_on = 0
+
+def turn_hdmi_off():
+    global hdmi_on
+    hdmi_on = 0
+    print('turn hdmi off')
+
+def turn_hdmi_on():
+    global hdmi_on
+    hdmi_on = 1
+    print('turn hdmi on')
+
+def both_method(channel):
+    global timer_thread
+    global turn_hdmi_off
+    global hdmi_on
+    if timer_thread != None:
+        timer_thread.cancel()
+
+    input_signal = GPIO.input(GPIO_PIR)
+    if input_signal == 0 and hdmi_on == 1:
+        timer_thread = threading.Timer(30, turn_hdmi_off)
+
+    if input_signal == 1 and hdmi_on == 0:
+        timer_thread = threading.Timer(5, turn_hdmi_on)
+
+    if timer_thread is not None:
+        if not timer_thread.is_alive():
+            timer_thread.start()
+    print(input_signal)
+
+GPIO.add_event_detect(GPIO_PIR, GPIO.BOTH, callback = both_method, bouncetime = 250)
+
+try:
+    while True:
+        time.sleep(60)
+except KeyboardInterrupt:
+    GPIO.cleanup()
