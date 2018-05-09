9711636c024e497cf937bc39c4090a8c33c38b31
Christian Decker <christian.decker@decker-software-solutions.de>
2018-05-09 19:45:38 +0200

    add methods for turn on and off condition

--- motion_detector.py
+++ motion_detector.py
@@ -14,6 +14,21 @@ GPIO.setup(GPIO_PIR, GPIO.IN)
 timer_thread = None
 hdmi_on = 0
 
+def both_method(channel):
+    global timer_thread
+    if timer_thread != None:
+        timer_thread.cancel()
+
+    if is_turn_on_condition():
+        timer_thread = threading.Timer(30, turn_hdmi_off)
+
+    if is_turn_off_condition():
+        timer_thread = threading.Timer(5, turn_hdmi_on)
+
+    if timer_thread is not None:
+        if not timer_thread.is_alive():
+            timer_thread.start()
+
 def turn_hdmi_off():
     global hdmi_on
     hdmi_on = 0
@@ -24,23 +39,15 @@ def turn_hdmi_on():
     hdmi_on = 1
     call("vcgencmd display_power 1", shell=True)
 
-def both_method(channel):
-    global timer_thread
-    global turn_hdmi_off
+def is_turn_on_condition():
     global hdmi_on
-    if timer_thread != None:
-        timer_thread.cancel()
-
     input_signal = GPIO.input(GPIO_PIR)
-    if input_signal == 0 and hdmi_on == 1:
-        timer_thread = threading.Timer(30, turn_hdmi_off)
+    return input_signal == 0 and hdmi_on == 1
 
-    if input_signal == 1 and hdmi_on == 0:
-        timer_thread = threading.Timer(5, turn_hdmi_on)
-
-    if timer_thread is not None:
-        if not timer_thread.is_alive():
-            timer_thread.start()
+def is_turn_off_condition():
+    global hdmi_on
+    input_signal = GPIO.input(GPIO_PIR)
+    return input_signal == 1 and hdmi_on == 0
 
 GPIO.add_event_detect(GPIO_PIR, GPIO.BOTH, callback = both_method, bouncetime = 250)
 
