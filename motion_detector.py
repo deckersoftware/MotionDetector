8aa7b8755da74770d2f15ceddad8cffd5aef6a9d
Christian Decker <christian.decker@decker-software-solutions.de>
2018-05-10 11:12:03 +0200

    add pylint

--- motion_detector.py
+++ motion_detector.py
@@ -1,37 +1,44 @@
 #!/usr/bin/python3
 
-import RPi.GPIO as GPIO
 import time
 import threading
+import RPi.GPIO as GPIO
 from config import configuration
 from display import display
 
-config_data = configuration().get_config_data()
-display_object = display(config_data)
+CONFIG_DATA = configuration().get_config_data()
+DISPLAY_OBJECT = display(CONFIG_DATA)
 
 GPIO.setmode(GPIO.BOARD)
-GPIO_PIR = config_data['motion_input_pin']
+GPIO_PIR = CONFIG_DATA['motion_input_pin']
 GPIO.setup(GPIO_PIR, GPIO.IN)
 
-timer_thread = None
+TIMER_THREAD = None
 
 def motion_detection_event(channel):
-    global timer_thread
-    if timer_thread != None:
-        timer_thread.cancel()
+    del channel
+    global TIMER_THREAD
+    if TIMER_THREAD != None:
+        TIMER_THREAD.cancel()
 
     input_signal = GPIO.input(GPIO_PIR)
-    if display_object.is_turn_on_condition(input_signal):
-        timer_thread = threading.Timer(config_data['display_turn_on_duration_in_seconds'], display_object.turn_display_off)
+    if DISPLAY_OBJECT.is_turn_on_condition(input_signal):
+        TIMER_THREAD = threading.Timer( \
+            CONFIG_DATA['display_turn_on_duration_in_seconds'], \
+            DISPLAY_OBJECT.turn_display_off \
+            )
 
-    if display_object.is_turn_off_condition(input_signal):
-        timer_thread = threading.Timer(config_data['display_turn_off_duration_in_seconds'], display_object.turn_display_on)
+    if DISPLAY_OBJECT.is_turn_off_condition(input_signal):
+        TIMER_THREAD = threading.Timer( \
+            CONFIG_DATA['display_turn_off_duration_in_seconds'], \
+            DISPLAY_OBJECT.turn_display_on \
+            )
 
-    if timer_thread is not None:
-        if not timer_thread.is_alive():
-            timer_thread.start()
+    if TIMER_THREAD is not None:
+        if not TIMER_THREAD.is_alive():
+            TIMER_THREAD.start()
 
-GPIO.add_event_detect(GPIO_PIR, GPIO.BOTH, callback = motion_detection_event, bouncetime = 250)
+GPIO.add_event_detect(GPIO_PIR, GPIO.BOTH, callback=motion_detection_event, bouncetime=250)
 
 try:
     while True:
