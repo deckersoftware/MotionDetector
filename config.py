ce039eff2f7ecf9f620381427939294a4d5744f2
Christian Decker <christian.decker@decker-software-solutions.de>
2018-08-06 20:30:10 +0200

    create config file if not exists

--- config.py
+++ config.py
@@ -1,8 +1,13 @@
 import os
 import json
+from shutil import copyfile
 
 class Configuration:
     _config_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'config.json')
+
+    if not os.path.isfile(_config_path):
+        _config_sample_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'config.json.sample')
+        copyfile(_config_sample_path, _config_path)
 
     def get_config_data(self):
         with open(self._config_path, 'r') as config_file:
